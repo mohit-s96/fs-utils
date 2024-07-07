49043fd4dd2ee124f9e14a896cd2425aaa2e3ca0
mohit-s96 <mohitsrivastava67@gmail.com>
2024-07-08 01:07:52 +0530

    colors

---
+++ colors.c
@@ -0,0 +1,54 @@
+void print_red()
+{
+    printf("\033[31m");
+}
+
+void print_green()
+{
+    printf("\033[32m");
+}
+
+void print_yellow()
+{
+    printf("\033[33m");
+}
+
+void print_blue()
+{
+    printf("\033[34m");
+}
+
+void print_magenta()
+{
+    printf("\033[35m");
+}
+
+void print_cyan()
+{
+    printf("\033[36m");
+}
+
+void print_white()
+{
+    printf("\033[37m");
+}
+
+void print_bold()
+{
+    printf("\033[1m");
+}
+
+void print_underlined()
+{
+    printf("\033[4m");
+}
+
+void print_invisible()
+{
+    printf("\033[08m");
+}
+
+void print_reset()
+{
+    printf("\033[0m");
+}
--- main.c
+++ main.c
@@ -1,21 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include "cli.h"
+#include "colors.c"
 
 int main(int argc, char **argv)
 {
     Cli_args *args = parse_cli(argc, argv);
-    printf("command => %d\n", args->command);
-    printf("path => %s\n", args->path);
-    printf("destination => %s\n", args->destination);
-    printf("source => %s\n", args->source);
-    printf("-s => %d\n", args->sort_by_size);
-    printf("-a => %d\n", args->sort_by_name);
-    printf("new dir => %s\n", args->new_dir_name);
-    printf("new file => %s\n", args->new_file_name);
-    printf("-nr => %d\n", args->no_recurse);
-    printf("search pattern => %s\n", args->search_pattern);
+    if (args->command != LS)
+        return 1;
 
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(args->path);
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            if (dir->d_type == DT_DIR)
+            {
+                print_cyan();
+                printf("%s\n", dir->d_name);
+                print_reset();
+            }
+            else if (dir->d_type == DT_LNK)
+            {
+                print_underlined();
+                printf("%s\n", dir->d_name);
+                print_reset();
+            }
+            else if (dir->d_type == DT_UNKNOWN)
+            {
+                print_red();
+                printf("%s\n", dir->d_name);
+                print_reset();
+            }
+            else
+            {
+                printf("%s\n", dir->d_name);
+            }
+        }
+        closedir(d);
+    }
+    else
+    {
+        print_red();
+        printf("'%s' is not a valid directory path\n", args->path);
+        print_reset();
+    }
     free(args);
     return 0;
 }
