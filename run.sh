25028b72ca16f5a68de01b27e2e9aa8dbd516822
mohit-s96 <mohitsrivastava67@gmail.com>
2024-10-14 02:29:13 +0530

    remove old scripts

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Compile with debug symbols (-g)
-# Disable library validation (-fno-sanitize=safe-stack)
-# Include all warning messages (-Wall)
-# Treat warnings as errors (-Werror)
-
-clang -g -fno-sanitize=safe-stack -Wall -Werror -o main.out main.c cli.c utils.c ls.c arena.c colors.c
--- test.sh
+++
@@ -1,3 +0,0 @@
-rm -f tests/test.out
-clang -o tests/test.out tests/test.c cli.c arena.c -lcmocka
-./tests/test.out
