80df820e25d942c11d91808f60403e30dbceba5e
mohit-s96 <mohitsrivastava67@gmail.com>
2024-10-29 19:31:20 +0530

    rewrite arena with linked lists

--- arena.c
+++ arena.c
@@ -1,67 +1,103 @@
+#include <pthread.h>
 #include "arena.h"
 
 void *init_arena(Arena *arena, size_t size)
 {
     void *start = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-
     if (start == MAP_FAILED)
     {
         return NULL;
     }
-    arena->start = start;
-    arena->size = 0;
-    arena->capacity = size;
+    Block *head_block = (Block *)malloc(sizeof(Block));
+    head_block->capacity = size;
+    head_block->next = NULL;
+    head_block->size = 0;
+    head_block->start = start;
 
-    return start;
+    arena->head = head_block;
+    arena->num_blocks = 1;
+    arena->tail = NULL;
+
+    // Initialize the mutex for this arena
+    pthread_mutex_init(&arena->mutex, NULL);
+
+    return NULL;
 }
 
-void *grow_arena(Arena *arena, size_t size)
+void grow_arena(Arena *arena, size_t size)
 {
-    if (size <= arena->capacity)
-    {
-        return arena->start;
-    }
-
     void *start = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
     if (start == MAP_FAILED)
     {
-        return NULL;
+        return;
     }
+    Block *new_block = (Block *)malloc(sizeof(Block));
+    new_block->capacity = size;
+    new_block->next = NULL;
+    new_block->size = 0;
+    new_block->start = start;
 
-    memcpy(start, arena->start, arena->size);
-    int success = munmap(arena->start, arena->capacity);
-    if (success != 0)
+    arena->num_blocks++;
+    if (arena->tail != NULL)
     {
-        fprintf(stderr, "Couldn't unmap the arena");
-        return NULL;
+        arena->tail->next = new_block;
     }
-
-    arena->start = start;
-    arena->capacity = size;
-
-    return start;
+    else
+    {
+        arena->head->next = new_block;
+    }
+    arena->tail = new_block;
 }
 
 void *allocate(Arena *arena, size_t size)
 {
-    if (arena->size + size > arena->capacity)
+    pthread_mutex_lock(&arena->mutex);
+    Block *block = arena->head;
+    unsigned long long total_block_size = 0;
+    while (block != NULL)
     {
-        // double
-        return grow_arena(arena, arena->capacity * 2);
+        if (block->size + size < block->capacity)
+        {
+            void *free_space = (char *)block->start + block->size;
+            block->size += size;
+            pthread_mutex_unlock(&arena->mutex);
+            return free_space;
+        }
+        total_block_size += block->size;
+        block = block->next;
     }
 
-    void *address = (char *)arena->start + arena->size;
-    arena->size += size;
+    // reaching here means needs more memory
 
-    return address;
+    // double the size
+    unsigned long long average_block_size = total_block_size / arena->num_blocks;
+    grow_arena(arena, (average_block_size + size) * 2);
+    arena->tail->size += size;
+
+    pthread_mutex_unlock(&arena->mutex);
+
+    return arena->tail->start;
 }
 
 void destroy(Arena *arena)
 {
-    int success = munmap(arena->start, arena->capacity);
-    if (success != 0)
+    pthread_mutex_lock(&arena->mutex);
+    Block *block = arena->head;
+    while (block != NULL)
     {
-        fprintf(stderr, "Couldn't unmap the arena");
+        int success = munmap(block->start, block->capacity);
+
+        if (success != 0)
+        {
+            fprintf(stderr, "Couldn't unmap the arena");
+        }
+
+        Block *next = block->next;
+        free(block);
+        block = next;
     }
+
+    pthread_mutex_unlock(&arena->mutex);
+    pthread_mutex_destroy(&arena->mutex);
 }
--- arena.h
+++ arena.h
@@ -4,15 +4,25 @@
 #include <sys/mman.h>
 #include <string.h>
 #include <stdio.h>
+#include <pthread.h>
 
-typedef struct
+typedef struct Block
 {
     void *start;
     size_t size;
     size_t capacity;
+    struct Block *next;
+} Block;
+
+typedef struct
+{
+    Block *head;
+    Block *tail;
+    int num_blocks;
+    pthread_mutex_t mutex;
 } Arena;
 
 void *init_arena(Arena *arena, size_t size);
-void *grow_arena(Arena *arena, size_t size);
+void grow_arena(Arena *arena, size_t size);
 void *allocate(Arena *arena, size_t size);
 void destroy(Arena *arena);
