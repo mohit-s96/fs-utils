fbf348c22cf1e023fddb49558c24376e1be936a5
mohit-s96 <mohitsrivastava67@gmail.com>
2024-11-16 19:39:14 +0530

    multi threaded copy

--- copy.c
+++ copy.c
@@ -1,8 +1,24 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <dirent.h>
+#include <unistd.h>
 #include "commands.h"
 #include "utils.h"
+#include "threads.h"
+
+typedef struct
+{
+    bool flag;
+    int exit_code;
+    pthread_mutex_t *mutex;
+    Arena *arena;
+} WorkerArgs;
+
+typedef struct
+{
+    char *source;
+    char *destination;
+} Job;
 
 int copy_file(FILE *f_source, FILE *f_dest)
 {
@@ -51,7 +67,166 @@ bool is_parent_dir_valid(char *parent_dir)
     return false;
 }
 
-int copy_dir_or_file(char *source, char *destination, bool flag, Arena *arena)
+void fail_all_tasks(WorkerArgs *worker_args)
+{
+    pthread_mutex_lock(get_queue_mutex());
+    set_active_tasks(0);
+    set_queue_size(0);
+    set_done(true);
+    pthread_cond_broadcast(get_not_empty_condition());
+    pthread_mutex_unlock(get_queue_mutex());
+    pthread_mutex_lock(worker_args->mutex);
+    worker_args->exit_code = EXIT_FAILURE;
+    pthread_mutex_unlock(worker_args->mutex);
+}
+
+void *work_copy(void *arg)
+{
+    DIR *dir;
+    Job *job;
+    Tuple t = {0};
+    struct dirent *d;
+    struct stat st_source, st_dest;
+    bool dir_name_provided = false;
+
+    WorkerArgs *worker_args = (WorkerArgs *)arg;
+    Arena *arena = worker_args->arena;
+
+    while ((job = (Job *)dequeue()) != NULL)
+    {
+        char *source = job->source;
+        char *destination = job->destination;
+
+        if (lstat(source, &st_source) != 0)
+        {
+            fprintf(stderr, "Error: Invalid source: %s\n", source);
+            fail_all_tasks(worker_args);
+            return NULL;
+        }
+        if (lstat(destination, &st_dest) != 0)
+        {
+            parent_path_from_child(destination, strlen(destination), &t, arena);
+            dir_name_provided = is_parent_dir_valid(t.parent);
+            if (!dir_name_provided)
+            {
+                fprintf(stderr, "Error: Invalid destination: %s\n", destination);
+                fail_all_tasks(worker_args);
+                return NULL;
+            }
+        }
+
+        bool is_source_dir = (st_source.st_mode & S_IFMT) == S_IFDIR;
+        bool is_dest_dir = (st_dest.st_mode & S_IFMT) == S_IFDIR;
+
+        if (is_source_dir && !is_dest_dir && !dir_name_provided)
+        {
+            fprintf(stderr, "Error: Can't copy directory to a file\n");
+            fail_all_tasks(worker_args);
+            return NULL;
+        }
+
+        if (strcmp(source, destination) == 0)
+        {
+            fprintf(stderr, "Error: Source and destination are identical\n");
+            fail_all_tasks(worker_args);
+            return NULL;
+        }
+
+        if (!is_source_dir)
+        {
+            FILE *f_source = fopen(source, "rb");
+            FILE *f_dest = is_dest_dir ? fopen(join_paths(destination, source, arena), "wb") : fopen(destination, "wb");
+            int success = copy_file(f_source, f_dest);
+            if (success != EXIT_SUCCESS)
+            {
+                fail_all_tasks(worker_args);
+                return NULL;
+            }
+        }
+        else
+        {
+            char *special_dir = parse_special_dir(source, strlen(source));
+            if (dir_name_provided)
+            {
+                int success = mkdir(destination, 0700);
+                if (success != 0)
+                {
+                    fprintf(stderr, "Error copying directory\n");
+                    fail_all_tasks(worker_args);
+                    return NULL;
+                }
+            }
+            else
+            {
+                if (special_dir == NULL)
+                {
+                    destination = !worker_args->flag
+                                      ? join_paths(destination, parent_path_from_child(source, strlen(source), &t, arena)->child, arena)
+                                      : destination;
+                    int success = mkdir(destination, 0700);
+                    if (success != 0 && errno != EEXIST)
+                    {
+                        fprintf(stderr, "Error copying directory\n");
+                        fail_all_tasks(worker_args);
+                        return NULL;
+                    }
+                }
+            }
+            dir = opendir(source);
+            if (dir == NULL)
+            {
+                fprintf(stderr, "Error opening directory\n");
+                fail_all_tasks(worker_args);
+                return NULL;
+            }
+            if (!worker_args->flag)
+            {
+                pthread_mutex_lock(worker_args->mutex);
+                worker_args->flag = true;
+                pthread_mutex_unlock(worker_args->mutex);
+            }
+            while ((d = readdir(dir)) != NULL)
+            {
+                if (!check_if_parent_dir(d->d_name))
+                {
+                    char *new_source = join_paths(source, d->d_name, arena);
+                    char *new_destination = join_paths(destination, d->d_name, arena);
+                    if (d->d_type == DT_DIR)
+                    {
+                        Job *j = (Job *)allocate(arena, sizeof(Job));
+                        j->source = new_source;
+                        j->destination = new_destination;
+                        enqueue(j);
+                    }
+                    else
+                    {
+                        FILE *f_source = fopen(new_source, "rb");
+                        FILE *f_dest = fopen(new_destination, "wb");
+                        int success = copy_file(f_source, f_dest);
+                        if (success != EXIT_SUCCESS)
+                        {
+                            fail_all_tasks(worker_args);
+                            return NULL;
+                        }
+                    }
+                }
+            }
+            closedir(dir);
+        }
+        pthread_mutex_lock(get_queue_mutex());
+        set_active_tasks(get_active_tasks() - 1);
+        // If no tasks are left, signal all threads to exit
+        if (get_active_tasks() == 0 && get_queue_size() == 0)
+        {
+            set_done(true);
+            pthread_cond_broadcast(get_not_empty_condition()); // Notify all waiting threads
+        }
+        pthread_mutex_unlock(get_queue_mutex());
+    }
+    return NULL;
+}
+
+int copy_dir_or_file_recursive(char *source, char *destination, bool flag, Arena *arena)
 {
     struct stat st_source, st_dest;
     Tuple t = {0};
@@ -97,27 +272,6 @@ int copy_dir_or_file(char *source, char *destination, bool flag, Arena *arena)
     }
     else
     {
-        /**
-         * CASE A: source ends with special directory i.e. "." or ".."
-         *
-         *         in this case we check if the destination dir name
-         *         was provided. if so, we move all the contents to the
-         *         destination under the provided dir name. note that
-         *         the provided dir name is one which doesn't exist &
-         *         needs to be created. if the destination dir provided
-         *         already exists, we copy all the contents to the top
-         *         level of the pre-existing directory.
-         *
-         * CASE B: source ends with normal directory name
-         *
-         *         in this case we check if the dest dir name has been
-         *         provided. if it is then we copy the contents from
-         *         source to the destination under the provided dir
-         *         name. otherwise, the content is copied under the
-         *         under a directory with the same name as the source
-         *         dir.
-         */
-
         char *special_dir = parse_special_dir(source, strlen(source));
         if (dir_name_provided)
         {
@@ -154,7 +308,7 @@ int copy_dir_or_file(char *source, char *destination, bool flag, Arena *arena)
         {
             if (!check_if_parent_dir(d->d_name))
             {
-                int status = copy_dir_or_file(join_paths(source, d->d_name, arena), join_paths(destination, d->d_name, arena), flag, arena);
+                int status = copy_dir_or_file_recursive(join_paths(source, d->d_name, arena), join_paths(destination, d->d_name, arena), flag, arena);
                 if (status != EXIT_SUCCESS)
                 {
                     return EXIT_FAILURE;
@@ -167,10 +321,45 @@ int copy_dir_or_file(char *source, char *destination, bool flag, Arena *arena)
     return EXIT_SUCCESS;
 }
 
+int copy_dir_or_file_threaded(char *source, char *destination, Arena *arena)
+{
+    long number_of_processors = sysconf(_SC_NPROCESSORS_ONLN);
+    pthread_t threads[number_of_processors];
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    Job j;
+    j.source = source;
+    j.destination = destination;
+
+    WorkerArgs args = {.arena = arena, .exit_code = 0, .flag = false, .mutex = &mutex};
+    enqueue(&j);
+
+    for (int i = 0; i < number_of_processors; i++)
+    {
+        if (pthread_create(&threads[i], NULL, work_copy, &args) != 0)
+        {
+            fprintf(stderr, "Failed to create thread %d\n", i);
+            return EXIT_FAILURE;
+        }
+    }
+
+    for (int i = 0; i < number_of_processors; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    pthread_mutex_destroy(get_queue_mutex());
+    pthread_cond_destroy(get_not_empty_condition());
+    pthread_cond_destroy(get_not_full_condition());
+
+    return args.exit_code;
+}
+
 int command_copy(Cli_args *args, Arena *arena)
 {
     char *source = args->source;
     char *destination = args->destination;
 
-    return copy_dir_or_file(source, destination, false, arena);
+    return copy_dir_or_file_threaded(source, destination, arena);
+    // return copy_dir_or_file_recursive(source, destination, false, arena);
 }
